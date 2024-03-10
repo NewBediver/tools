b27fda547fe26d478f9cae748ac278bfd26b9fb3
Vladislav Serikov <serikov_v1999@mail.ru>
2024-03-10 11:27:14 +0300

    Add setup llvm script.

---
+++ setup-llvm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Enter llvm version number"
+    exit 1
+fi
+
+# step 1: download llvm installation script
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+
+# step 2: install llvm with preferred version
+sudo ./llvm.sh $1
+
+# step 3: set clang version as the default compiler
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-$1 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$1 100
+
+# step 4: remove llvm.sh script
+rm llvm.sh
